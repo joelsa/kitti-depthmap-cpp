c96f80e2fbf8b47557bdcef941ab42d6a265a234
Joel Schulz-Andres <joel.schulz-andres@gmx.de>
2020-09-28 00:23:37 +0200

    Make indentation consistent

--- src/Calibration.h
+++ src/Calibration.h
@@ -1,19 +1,19 @@
 #include <Eigen/Dense>
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 #include <vector>
 
 class Calibration {
-  public:
-    Eigen::Affine3f velo2cam;
-    Eigen::Matrix3f cam2cam;
+public:
+  Eigen::Affine3f velo2cam;
+  Eigen::Matrix3f cam2cam;
 
-    void loadVelo2Cam(std::string file);
-    void loadCam2Cam(std::string file, std::string camera);
+  void loadVelo2Cam(std::string file);
+  void loadCam2Cam(std::string file, std::string camera);
 
-  private:
-    std::vector<std::string> split(const std::string &s, char delim);
-    std::vector<float> getValuesByKey(const std::string file, const std::string inKey);
-  
+private:
+  std::vector<std::string> split(const std::string &s, char delim);
+  std::vector<float> getValuesByKey(const std::string file,
+                                    const std::string inKey);
 };
